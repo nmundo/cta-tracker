552feab1769aa5047ff207dcc5f5983b9bf799d0
Nathan Mundo <nathanmundo@icloud.com>
2025-10-30 15:02:31 -0500

    cleaned up UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 			staNm: string;
 			stpDe: string;
 			rn: string;
-			rt: string;
+			rt: 'Red' | 'Blue' | 'Brn' | 'G' | 'Org' | 'P' | 'Pink' | 'Y' | 'Pexp';
 			destSt: string;
 			destNm: string;
 			trDr: string;
@@ -26,6 +26,17 @@
 		}[];
 	};
 
+	const LINES = {
+		Red: { name: 'Red', hex: '#FF4242' },
+		Blue: { name: 'Blue', hex: '#2D93AD' },
+		Brn: { name: 'Brown', hex: '#3D3522' },
+		G: { name: 'Green', hex: '#88AB75' },
+		Org: { name: 'Orange', hex: '#F26419' },
+		P: { name: 'Purple', hex: '#B497D6' },
+		Pink: { name: 'Pink', hex: '#FFB2E6' },
+		Y: { name: 'Yellow', hex: '#DBD56E' }
+	};
+
 	let mapId = $state('');
 	let trainData: TrainTrackerResponse | null = $state(null);
 
@@ -35,23 +46,37 @@
 			.then(({ ctatt }) => (trainData = ctatt))
 			.catch((e) => console.log(e));
 	};
+
+	const calcTimeDelta = (arrivalTime: string) => {
+		const time = new Date(arrivalTime);
+		const mins = Math.round((time.getTime() - Date.now()) / 1000 / 60);
+
+		return mins;
+	};
 </script>
 
-<input bind:value={mapId} />
-<button onclick={() => updateTimes()}>Submit</button>
+<input type="text" class="input" bind:value={mapId} />
+<button class="btn" onclick={() => updateTimes()}>Submit</button>
 {#if trainData}
 	{#if trainData.errNm}
 		<div>{trainData.errNm}</div>
 	{:else}
-		<table>
-			<tbody>
-				{#each trainData.eta as { destNm, arrT }}
-					<tr>
-						<td>{arrT}</td>
-						<td>{destNm}</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
+		<ul class="list rounded-box bg-base-100 shadow-md">
+			<li class="p-4 pb-2 text-2xl tracking-wide opacity-60">
+				Upcoming trains at {trainData.eta[0].staNm}
+			</li>
+			{#each trainData.eta as { destNm, arrT, rt, stpDe }}
+				<li class="list-row">
+					<div>
+						<div class="size-10 rounded-box" style="background-color: {LINES[rt].hex};"></div>
+					</div>
+					<div class="list-col-grow">
+						<div class="uppercase">{LINES[rt].name} LINE</div>
+						<div class="text-xs font-semibold opacity-60">{stpDe}</div>
+					</div>
+					<div class="text-4xl font-thin tabular-nums opacity-30">{calcTimeDelta(arrT)}</div>
+				</li>
+			{/each}
+		</ul>
 	{/if}
 {/if}
