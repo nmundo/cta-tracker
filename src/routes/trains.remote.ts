b98073512d069ae3c2394fc94aa668a8800ce590
Nathan Mundo <nathanmundo@icloud.com>
2025-11-05 16:53:22 -0600

    added remote fuction to get train stations. wip

--- src/routes/trains.remote.ts
+++ src/routes/trains.remote.ts
@@ -1,11 +1,17 @@
-import { query } from '$app/server';
-import { TRAIN_API_KEY } from '$env/static/private';
+import { query } from '$app/server'
+import { TRAIN_API_KEY } from '$env/static/private'
 
 export const getTrainTimes = query('unchecked', async (mapid: string) => {
 	const response = await fetch(
 		`https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=${TRAIN_API_KEY}&mapid=${mapid}&outputType=JSON`
-	);
+	)
 
-	const data: TrainTrackerResponse = await response.json();
-	return data.ctatt;
-});
+	const data: TrainTrackerResponse = await response.json()
+	return data.ctatt
+})
+
+export const getStations = query('unchecked', async () => {
+	const response = await fetch('https://data.cityofchicago.org/resource/8pix-ypme.json')
+
+	return response
+})
