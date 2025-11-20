4c6260109cce1a21ca92b5700f8f7ebaf8451006
Nathan Mundo <nathanmundo@icloud.com>
2025-11-20 15:54:58 -0600

    Squashed commit of the following:
    commit ec7a165257204ba25a24eec1ff44cf951e2d25f4
    Author: Nathan Mundo <nathanmundo@icloud.com>
    Date:   Thu Nov 20 15:54:20 2025 -0600
    
        animation and work on showing heading
    
    commit 24ed04ec6933656a445d83720daea4c73a31cee3
    Author: Nathan Mundo <nathanmundo@icloud.com>
    Date:   Thu Nov 20 15:33:28 2025 -0600
    
        Added geo location
    
    commit c1485721f45406b568cf40778d85ff6860166e14
    Author: Nathan Mundo <nathanmundo@icloud.com>
    Date:   Thu Nov 20 14:28:33 2025 -0600
    
        cleaning up code for readability
    
    commit df26c6fc6470f9061f9d0bba6f08185e8dad064c
    Author: Nathan Mundo <nathanmundo@icloud.com>
    Date:   Mon Nov 17 15:14:06 2025 -0600
    
        Update README.md
    
    commit c3f22d02135aa32b6ef9fddbdd112f12bd465f03
    Author: Nathan Mundo <nathanmundo@icloud.com>
    Date:   Mon Nov 17 14:28:22 2025 -0600
    
        Update .gitignore

--- src/routes/trains.remote.ts
+++ src/routes/trains.remote.ts
@@ -46,8 +46,8 @@ export const getStations = query(async (): Promise<StationStop[]> => {
 				station_name: station.station_name,
 				station_descriptive_name: station.station_descriptive_name,
 				branches: getBranches(station),
-				latitude: station.location.latitude,
-				longitude: station.location.longitude,
+				latitude: Number(station.location.latitude),
+				longitude: Number(station.location.longitude),
 				ada: station.ada,
 				lines: new Set(getLineArray(station))
 			})
